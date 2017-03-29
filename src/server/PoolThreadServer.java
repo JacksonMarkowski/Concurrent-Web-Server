39271c1952aa78ca6960c3daa49b94506deb4d45
JacksonMarkowski <markowski.jackson@gmail.com>
2017-03-29 15:59:02 -0600

    Implementation of thread pool server

--- src/server/PoolThreadServer.java
+++ src/server/PoolThreadServer.java
@@ -1,8 +1,26 @@
 package server;
 
-public class PoolThreadServer extends Server{
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PoolThreadServer extends Server {
+
+    private ExecutorService threadPool = Executors.newFixedThreadPool(5);
 
     public void run() {
+        createServerSocket();
 
+        while(isServerRunning()){
+            try {
+                Socket clientSocket = acceptClientSocket();
+                ClientHandler newHandler = new ClientHandler(this, clientSocket);
+                threadPool.execute(newHandler);
+            } catch (IOException e) {
+                //ToDo: Handle exception from streams
+            }
+        }
+        threadPool.shutdown();
     }
 }
