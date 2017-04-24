d267f72180d8add9b292b4e081c7de714be8e374
JacksonMarkowski <markowski.jackson@gmail.com>
2017-04-23 22:46:20 -0600

    Enqueue and Dequeue methods for logging class

--- src/server/Log.java
+++ src/server/Log.java
@@ -36,6 +36,47 @@ public class Log {
     }
 
     protected void logString(String str) {
-        //ToDO: implement logging queue
+        Node node = new Node();
+        node.str = str;
+        enq(node);
+    }
+
+    private void enq(Node node) {
+        while (true) {
+            Node last = tail.get();
+            Node next = last.next.get();
+            if (last == tail.get()) {
+                if (next == null) {
+                    if (last.next.compareAndSet(null, node)) {
+                        tail.compareAndSet(last, node);
+
+                        return;
+                    }
+                } else {
+                    tail.compareAndSet(last, next);
+                }
+            }
+        }
+    }
+
+    public void deq() throws Exception {
+        while (true) {
+            Node first = head.get();
+            Node last = tail.get();
+            Node next = first.next.get();
+            if (first == head.get()) {
+                if (first == last) {
+                    if (next == null) {
+                        throw new Exception();
+                    }
+                    tail.compareAndSet(last, next);
+                } else {
+                    String str = next.str;
+                    if (head.compareAndSet(first, next)) {
+                        logger.info(str);
+                    }
+                }
+            }
+        }
     }
 }
